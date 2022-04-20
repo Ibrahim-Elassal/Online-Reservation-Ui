b98f9e51757288c156f890e4bf8660e54920cee1
“ibrahim” <ibrahimelassal662@gmail.com>
2022-04-20 02:06:52 +0200

    delete project

--- online_reservation_ui/android/app/src/main/java/com/ibrahimelassalonline_reservation_ui/online_reservation_ui/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.ibrahimelassalonline_reservation_ui.online_reservation_ui;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
